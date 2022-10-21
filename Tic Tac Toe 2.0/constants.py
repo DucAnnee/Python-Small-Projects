f9d6de4e932c8eabf9f61ce7420527c4fec03828
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2022-10-21 16:17:18 +0700

    Update constants.py

--- Tic Tac Toe 2.0/constants.py
+++ Tic Tac Toe 2.0/constants.py
@@ -16,7 +16,7 @@ cross_length = 55
 cir_offset = square_size / 2
 cross_offset = 57.5
 
-LINE_COLOUR = (17, 138, 104)
-BG_COLOUR = (136, 245, 214)
-CIRC_COLOUR = (20, 140, 150)
-CROSS_COLOUR = (20, 140, 150)
+LINE_COLOUR = (17, 78, 74)
+BG_COLOUR = (36, 126, 105)
+CIRC_COLOUR = (2, 170, 170)
+CROSS_COLOUR = (155, 245, 245)
